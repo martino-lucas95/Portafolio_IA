645dbc39b9bedf9710541f07191fc5b7f82572c4
LUCAS MARTINO <LUCAS.MARTINO@correo.ucu.edu.uy>
2024-09-13 18:15:34 -0300

    add static files routes

--- ml_portfolio/settings.py
+++ ml_portfolio/settings.py
@@ -120,9 +120,13 @@ STATIC_URL = '/static/'
 # Define STATIC_ROOT
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, 'static'),
-# ]
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'staticfiles'),
+]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
