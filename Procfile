27794181366c5997dbd83b45faa1eaddfa5dd349
LUCAS MARTINO <LUCAS.MARTINO@correo.ucu.edu.uy>
2024-09-13 17:42:50 -0300

    modify db

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn ml_portfolio.wsgi
--- ml_portfolio/settings.py
+++ ml_portfolio/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -24,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-v*hez7$e@xvj4m#^q4#a33c(%4bw53*!8lc6=$263j7zv=-5=0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -76,10 +77,7 @@ WSGI_APPLICATION = 'ml_portfolio.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
 }
 
 
