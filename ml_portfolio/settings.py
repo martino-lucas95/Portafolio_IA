eb7d6c34dde718792ec37c51fa857ef1b6f49f68
LUCAS MARTINO <LUCAS.MARTINO@correo.ucu.edu.uy>
2024-09-13 18:22:29 -0300

    modify settings

--- ml_portfolio/settings.py
+++ ml_portfolio/settings.py
@@ -121,7 +121,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'staticfiles'),
+    os.path.join(BASE_DIR, 'static'),
 ]
 
 MEDIA_URL = '/media/'
